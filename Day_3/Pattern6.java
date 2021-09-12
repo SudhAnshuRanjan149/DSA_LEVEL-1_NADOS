0506ad0a200fed2e2822e200c85c53e7049f895a
Sudhanshu Ranjan <sudhanshuranjan149@gmail.com>
2021-09-12 15:00:08 +0530

    pattern questions

--- Day_3/Pattern6.java
+++ Day_3/Pattern6.java
@@ -5,22 +5,28 @@ public class Pattern6 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        // write ur code here
 
+        // initialize nstars and nspaces
         int nstars = n / 2 + 1;
         int nspaces = 1;
+
         for (int i = 1; i <= n; i++) {
+            // prints stars
             for (int j = 1; j <= nstars; j++) {
                 System.out.print("*\t");
             }
+            // print spaces
             for (int j = 1; j <= nspaces; j++) {
                 System.out.print("\t");
             }
+            // print stars
             for (int j = 1; j <= nstars; j++) {
                 System.out.print("*\t");
             }
+            // enter
             System.out.println();
 
+            // update nstars and nspaces
             if (i <= n / 2) {
                 nstars--;
                 nspaces += 2;
--- Day_3/Pattern9.java
+++ Day_3/Pattern9.java
@@ -5,15 +5,17 @@ public class Pattern9 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        // write ur code here
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-                if(i==j || i+j==n+1){
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                // condition --> print stars only when i==j or i+j=n+1
+                if (i == j || i + j == n + 1) {
                     System.out.print("*\t");
-                }else{
+                } else {
                     System.out.print("\t");
                 }
             }
+            // enter
             System.out.println();
         }
     }
